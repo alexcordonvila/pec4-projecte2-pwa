8bd6852aea62b0e24ddfd5cfc64d519f12676392
Alex Cordon <alexcordonvila@gmail.com>
2025-12-12 22:12:58 +0100

    Crear componentes list, detail y service de movies

---
+++ pwa/src/app/pages/movies-detail/movies-detail.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movies-detail',
+  imports: [],
+  templateUrl: './movies-detail.html',
+  styleUrl: './movies-detail.scss',
+})
+export class MoviesDetail {
+
+}
