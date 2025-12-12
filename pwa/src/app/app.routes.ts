a2d6a34e281acd3174875aa92cb430ce1cd00afa
Alex Cordon <alexcordonvila@gmail.com>
2025-12-12 22:25:02 +0100

    Implementamos un principio de movies list

--- pwa/src/app/app.routes.ts
+++ pwa/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { MoviesDetail } from './pages/movies-detail/movies-detail';
+import { MoviesList } from './pages/movies-list/movies-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'movies', pathMatch: 'full'},
+    {path: 'movies', component: MoviesList},
+    {path: 'movies/:id', component: MoviesDetail},
+    
+
+
+];
